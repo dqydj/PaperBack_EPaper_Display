f877110ddf418cf8b2ed9549f5da2663a61da9eb
PK <pk@dqydj.com>
2017-06-10 20:25:37 -0700

    Add Python image conversion script

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# PaperBack_EPaper_Display
-Internet Connected or Standard Input EPaper Display 
+# PaperBack EPaper Display
+
+PaperBack is an internet connected EPaper Display.  PaperBack is an entry in the 2017 Hackaday Prize, you can see a more complete (and verbose!) [writeup here](https://hackaday.io/project/21607-paperback-desktop-e-paper-monitor).
+
+Eventually, I plan on having a standard input (VGA? DVI?) onboard to drive an ePaper Display as a second monitor at a maximum of somewhere around 1 frame per second.  The current version is driven by an [ESP32](https://espressif.com/en/products/hardware/esp32/overview) and is updatable over the air.
+
+## Hardware Overview
+
+Currently, PaperBack exists as a single unpublished PCB.  I had to bodge together the first draft due to a mistake in construction.
+
+Expect an update soon for a second PCB spin.
+
+Hardware Overview:
+
+* Espressif ESP32 Dev Board
+* ED060SC4 // LB060S01 // LB060S04 6" ePaper Display
+    * I plan to test with a ED097OC1 9.7" ePaper Display as Well
+* 800x600 Maximum Resolution
+    * 1200x825 Maximum if the ED097OC1 works.
+* 16 Color (Greyscale) Support / 4-Bit Grey
+* Around 1 second for a fullscreen update on 800x600
+
+## Software/Firmware Overview
+
+This first draft has a few firmware versions to choose from for various functionality.  You can find them in `/firmware`.  The most basic firmware (suggested to start) displays a single static image on an ePaper Display.
+
+The Python utility `convert.py` in `/software/image_converter` automatically converts images which are compatible with Paperback.
+
+## Licensing and Other
+
+* My main site: [DQYDJ](https://dqydj.com)
+* Where available, all of the code is licensed as MIT.
+* Where available, all of the hardware is licensed as MIT.
+* If needed, other licenses will be quoted for individual parts.
+* I would, of course, _love_ a link if you use any assets - but feel free to use everything as you wish.
---
+++ software/software/image_converter/convert.py
@@ -0,0 +1,115 @@
+"""Automatically convert and resize images to fit an E-Paper Display.
+
+This utility takes an image, automatically resizes and centers it, and changes
+the color depth to fit on Paperback.  The output file contains an array which
+can be copied and pasted directly into a header file in the static image
+greyscale firmware.
+
+Example usage:
+
+python convert.py -i paperback.png -o picture.h
+
+# or
+
+python convert.py -i paperback.png -p -o picture.h
+
+# to add a preview.
+
+Author: PK
+Site: https://hackaday.io/project/21607-paperback-desktop-e-paper-monitor
+License: MIT
+"""
+from PIL import Image, ImageOps
+from optparse import OptionParser
+import sys
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+BYTE_LENGTH = 8
+
+
+def main(argv=None):
+    """Convert an image to fit on Paperback."""
+    parser = OptionParser()
+    parser.add_option('-i', action="store", dest="inputfile")
+    parser.add_option('-o', action="store", dest="outputfile")
+    parser.add_option(
+        "-p",
+        action="store_true",
+        dest="preview",
+        default=False
+    )
+
+    (opts, args) = parser.parse_args()
+
+    if opts.inputfile is not None:
+        inputfile = opts.inputfile
+    else:
+        print ("Use the -i option and pass an inputfile")
+        return 1
+
+    if opts.outputfile is not None:
+        outputfile = opts.outputfile
+    else:
+        print ("Use the -o option and pass an outputfile")
+        return 2
+
+    # Open the image, convert it to color, and fit it to the screen
+    im = Image.open(inputfile)
+    im = im.convert('RGB')
+    im.thumbnail((SCREEN_WIDTH, SCREEN_HEIGHT), Image.ANTIALIAS)
+
+    # If the image is smaller than 800x600 (likely), paste it on a white
+    # background equal to the screen size
+    if (SCREEN_WIDTH != im.size[0] or SCREEN_HEIGHT != im.size[1]):
+        background = Image.new(
+            im.mode,
+            (SCREEN_WIDTH, SCREEN_HEIGHT),
+            0xFFFFFF
+        )
+
+        img_w, img_h = im.size
+        bg_w, bg_h = background.size
+        offset_n = (int((bg_w - img_w) / 2), int((bg_h - img_h) / 2))
+
+        background.paste(im, offset_n)
+        im = background
+
+    pixels = im.load()
+    color_byte_list = []
+    color_byte = []
+
+    # Loop pixel by pixel to carefully construct a 4-Bit color depth
+    # image.  Also edit the pixels themselves.
+    for i in range(0, im.size[1]):
+        for k in range(0, im.size[0]):
+            r, g, b = im.getpixel((k, i))
+            avg_bright = ((r + g + b) // 3)
+            stripped = avg_bright & 0B11110000
+            color_byte.append("{0:04b}".format(stripped >> 4))
+
+            stripped = avg_bright & 0B11110000
+            pixels[k, i] = (int(stripped), int(stripped), int(stripped))
+
+            if len(color_byte) >= 2:
+                color_byte_list.append(''.join(color_byte))
+                color_byte = []
+
+    # Write out the output file.
+    with open(outputfile, 'w+') as f:
+        f.write(
+            "const uint8_t img_bytes[(800*600)/2] = {\n"
+        )
+        for entry in color_byte_list:
+            f.write("\t0B" + entry + ",\n")
+        f.write(
+            "};"
+        )
+
+    # If you use the -p flag, show a preview.
+    if opts.preview:
+        im.show()
+
+
+if __name__ == "__main__":
+    sys.exit(main())
